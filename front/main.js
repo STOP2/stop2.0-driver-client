f221c519bee37cb469a50b3cb94d9e9535c2f91c
Ville Talonpoika <talonvi@gmail.com>
2016-09-20 10:25:39 +0300

    UI:ta

--- front/main.js
+++ front/main.js
@@ -1,10 +1,8 @@
 var client = mqtt.connect("wss://test.mosca.io") // you add a ws:// url here
 
-client.subscribe("mqtt/demo")
+client.subscribe("data")
 
 client.on("message", function (topic, payload) {
   alert([topic, payload].join(": "))
   client.end()
 })
-
-//client.publish("mqtt/demo", "hello world!")
