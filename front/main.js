d2212e30207a2b8d34a3600c9a5d31e9c6dff4b7
Ville Talonpoika <talonvi@gmail.com>
2016-09-20 11:04:19 +0300

    Lisätty listan toiminnallisuus

--- front/main.js
+++ front/main.js
@@ -1,8 +1,28 @@
-var client = mqtt.connect("wss://test.mosca.io") // you add a ws:// url here
+// MQTT
+
+var client = mqtt.connect("wss://192.168.99.100:9001") // you add a ws:// url here
 
 client.subscribe("data")
 
 client.on("message", function (topic, payload) {
-  alert([topic, payload].join(": "))
+  console.log([topic, payload].join(": "))
+  addStop(topic,payload);
   client.end()
 })
+
+// UI-päivitys
+
+var currentStop;
+var emptyButton = document.querySelector(".stop-empty-button");
+emptyButton.addEventListener("click", function() {
+  // Lähetä tieto siitä ettei pysäkillä ollut ketään
+});
+
+var stopList = document.querySelector(".stop-list");
+
+function addStop(topic,payload) {
+  var item = document.createElement("li");
+  var node = document.createTextNode(payload);
+  item.appendChild(node);
+  stopList.appendChild(item);
+}
