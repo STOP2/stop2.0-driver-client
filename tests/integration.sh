7694e1a9ec51b6c1edaaec94df652f48f4a02468
Juri Liukkonen <juri.liukkonen@helsinki.fi>
2016-09-20 10:15:28 +0300

    Make tests run inside Docker

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,11 +1,14 @@
 #!/bin/sh -
 
+mosquitto -c /etc/mosquitto/mosquitto.conf &
+netstat -l | egrep '(1883|9001)'
 MSG="Hello, world!"
-
-sleep 2 && /usr/bin/mosquitto_pub -t testchannel -m "$MSG" &
+sleep 3 && /usr/bin/mosquitto_pub -t testchannel -m "$MSG" &
 RES=`/usr/bin/mosquitto_sub -C 1 -t testchannel`
 if [ "$RES" = "$MSG" ]; then
+	echo "Mosquitto is up and running."
 	exit 0
 else
+	echo "Mosquitto doesn't seem to be up."
 	exit 1
 fi
