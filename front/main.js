d762f77639555b83a8eb8bf480acf6fdb49573df
Juri Liukkonen <juri.liukkonen@helsinki.fi>
2016-09-30 07:04:28 +0300

    Oikat reittitiedot bussinumeron perusteella

--- front/main.js
+++ front/main.js
@@ -1,20 +1,89 @@
-var api = "http://test"
+const RT_API_URL = "http://dev.hsl.fi/hfp/journey/bus/";
+const BUS_ID = 1209;
+var api = "http://test";
 var mqttClient = mqtt.connect("ws://epsilon.fixme.fi:9001");
 
 var currentRoute = "";
-
+var tripData;
 var currentStop;
 var driverButton;
 var stopList;
 
 var stops = [];
-var stopLoadedData = [];
+
+function hslRealTimeAPIHandler() {
+  if (this.status == 200 && this.responseText) {
+    tripData = parseHSLRealTimeData(this.responseText);
+    var queryStr = `{
+      fuzzyTrip(route: "${tripData.line}", direction: ${tripData.direction}, date: "${tripData.date}", time: ${tripData.start})
+      { 
+        gtfsId
+        stops
+        { 
+          gtfsId
+          name
+        }
+        route
+        {
+          longName
+        }
+      }
+}`;
+
+    var r = new XMLHttpRequest();
+    r.onload = hslTripQueryHandler;
+    r.open("POST", "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", true);
+    r.setRequestHeader("Content-type", "application/graphql");
+    r.send(queryStr);
+  } else {
+    console.log("Connection to HSL real time API failed");
+  }
+}
+
+function parseHSLRealTimeData(str) {
+  var tmpobj = JSON.parse(str);
+  if (!tmpobj) {
+    return null;
+  }
+  tmpobj = tmpobj[Object.keys(tmpobj)[0]]["VP"];
+  var d = new Date(tmpobj.tst);
+  var strDate = d.getUTCFullYear();
+  var m = d.getMonth() + 1;
+  strDate += m < 10? "0" + m: m;
+  strDate += d.getUTCDate();
+  return {
+    vehicle: tmpobj.veh,
+    line: "HSL:" + tmpobj.line,
+    direction: tmpobj.dir - 1,
+    start: ((Math.floor(tmpobj.start / 100) * 60) + tmpobj.start % 100) * 60,
+    timeStr: tmpobj.tst,
+    date: strDate
+  }
+}
+
+function hslTripQueryHandler() {
+  if (this.status == 200 && this.responseText != null) {
+    var queryRes = JSON.parse(this.responseText).data.fuzzyTrip;
+    tripData.stops = queryRes.stops;
+    tripData.route = queryRes.route;
+    renderStops(tripData);
+  } else {
+    console.log("Connection to HSL GraphQL service failed");
+  }
+}
+
+function getCurrentVehicleData() {
+  var r = new XMLHttpRequest();
+  r.open("GET", RT_API_URL + BUS_ID + "/"); // asynchronous by default
+  r.onload = hslRealTimeAPIHandler;
+  r.send();
+}
 
 function init() {
-  currentRoute = document.querySelector("#route-name").value;
   createUI();
-  getStops();
+  getCurrentVehicleData();
 }
+
 
 function createUI() {
   document.querySelector(".content").innerHTML = `
@@ -24,7 +93,7 @@ function createUI() {
 
         <br />
 
-        <button class="driver-button">Kuljettajan nappi, kling</button>`
+        <button class="driver-button">Kuljettajan nappi, kling</button>`;
   driverButton = document.querySelector(".driver-button");
        driverButton.addEventListener("click", function() {
          postDriverButton();
@@ -33,51 +102,22 @@ function createUI() {
 }
 
 mqttClient.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "))
+  console.log([topic, payload].join(": "));
   addStop(JSON.parse(payload));
-})
+});
 
-function getStops() {
-  mqttClient.subscribe('stoprequests/' + currentRoute);
-
-  // Hae stopit
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          // Luo nodet
-          var s = xmlHttp.responseText.toString();
-          document.querySelector("h2").innerHTML = "Pysäkit (" + currentRoute + ")";
-          if (s !== undefined) {
-            stopLoadedData = JSON.parse(s);
-            for (var d of stopLoadedData.data.pattern.stops) {
-              var item = document.createElement("li");
-              item.classList.add("stop-" + d.name.replace(/\s/g, ''));
-              d.count = 0;
-              var color;
-              if (d.count === 0) {
-                color = "blue";
-              } else {
-                color = "red";
-              }
-              item.innerHTML = "<span class='run-animation'>" + d.name + " | <span style='font-weight: bold; color: " + color + ";'>" + d.count + "</span></span>";
-              stopList.appendChild(item);
-              d.node = item;
-              d.count = 1;
-              stops.push(d);
-            }
-          }
+function renderStops(trip) {
+  var t = trip.start / 60;
+  document.querySelector("h2").innerHTML = trip.route.longName + ", lähtö klo " + Math.floor(t / 60) + ":" + (t % 60);
+  for (var s of trip.stops) {
+    s.count = 0;
+    var item = document.createElement("li");
+    item.classList.add("stop-" + s.gtfsId);
+    item.innerHTML = "<span class='run-animation'>" + s.name + " | <span style='font-weight: bold; color: blue;'>" + s.count + "</span></span>";
+    stopList.appendChild(item);
+    stop.node = item;
+    stops.push(s);
   }
-  xmlHttp.open("POST", "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", true);
-  xmlHttp.setRequestHeader("Content-type", "application/graphql");
-  xmlHttp.send(`{
-    pattern(id:"HSL:1506:0:01") {
-        name
-        stops{
-          gtfsId
-          name
-        }
-      }
-    }`);
 }
 
 function addStop(payload) {
@@ -91,12 +131,7 @@ function addStop(payload) {
       } else if (payload.request_type=="stop") {
         s.count++;
       }
-      var color;
-      if (s.count === 0) {
-        color = "blue";
-      } else {
-        color = "red";
-      }
+      var color = s.count === 0? "blue": "red";
       s.node.innerHTML = "<span class='run-animation'>" + s.name + " | <span style='font-weight: bold; color: " + color + ";'>" + s.count + "</span></span>";
       return;
     }
@@ -108,3 +143,5 @@ function postDriverButton() {
   xhttp.open("POST", api + "/stoprequests", true);
   xhttp.send();
 }
+
+init();
--- front/test/test.js
+++ front/test/test.js
@@ -1,11 +1,12 @@
-describe('User', function() {
-  describe('#save()', function() {
-    it('should save without error', function(done) {
-      var user = new User('Luna');
-      user.save(function(err) {
-        if (err) done(err);
-        else done();
-      });
-    });
-  });
+//var chai = require('chai');
+//var assert = chai.assert;
+
+
+suite('HSL real time');
+
+test('parseHSLRealTimeData success', function() {
+  var s = '{"/hfp/journey/bus/1313/2550/2/XXX/1206/2222234/60;24/18/82/04":{"VP":{"desi":"2550","dir":"2","oper":"XXX","veh":"1313","tst":"2016-09-28T09:11:50.333Z","tsi":1475053910,"spd":0.56,"lat":60.1803,"long":24.8248,"dl":18,"oday":"XXX","jrn":"XXX","line":"2550","start":"1206"}}}';
+  var resObj = parseHSLRealTimeData(s);
+  var myObj = {"start": 1206, "line": "HSL:2550", "vehicle": 1313, "direction": 1, "time": 1206};
+  assert(myObj.line == resObj.line);
 });
