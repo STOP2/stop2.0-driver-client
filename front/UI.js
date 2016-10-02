165d93745e1b341e0875854b3e237b395c91660c
Ville Talonpoika <talonvi@gmail.com>
2016-10-02 16:13:49 +0300

    Koodi jaettu staattisiin luokkiin

---
+++ front/UI.js
@@ -0,0 +1,60 @@
+class UI {
+
+  static init() {
+    mqttClient.on("message", function (topic, payload) {
+      console.log([topic, payload].join(": "));
+      addStop(JSON.parse(payload));
+    });
+    UI.createUI();
+    NetworkHandler.getCurrentVehicleData();
+  }
+
+  static createUI() {
+    document.querySelector(".content").innerHTML = `
+          <h2>Pysäkit</h2>
+
+          <ul class="stop-list"></ul>
+
+          <br />
+
+          <button class="driver-button">Kuljettajan nappi, kling</button>`;
+    driverButton = document.querySelector(".driver-button");
+         driverButton.addEventListener("click", function() {
+           NetworkHandler.postDriverButton();
+         });
+    stopList = document.querySelector(".stop-list");
+  }
+
+  static renderStops(trip) {
+    var t = trip.start / 60;
+    document.querySelector("h2").innerHTML = trip.route.longName + ", lähtö klo " + Math.floor(t / 60) + ":" + (t % 60);
+    for (var s of trip.stops) {
+      s.count = 0;
+      var item = document.createElement("li");
+      item.classList.add("stop-" + s.gtfsId);
+      item.innerHTML = "<span class='run-animation'>" + s.name + " | <span style='font-weight: bold; color: blue;'>" + s.count + "</span></span>";
+      stopList.appendChild(item);
+      stop.node = item;
+      stops.push(s);
+    }
+  }
+
+  static addStop(payload) {
+    for (var s of stops) {
+      if (s.gtfsId == payload.stop_id) {
+        // Perutaan pysähdys
+        if (payload.request_type=="cancel") {
+          if (s.count == 0) return;
+          s.count--;
+        // Lisätään pysähdys
+        } else if (payload.request_type=="stop") {
+          s.count++;
+        }
+        var color = s.count === 0? "blue": "red";
+        s.node.innerHTML = "<span class='run-animation'>" + s.name + " | <span style='font-weight: bold; color: " + color + ";'>" + s.count + "</span></span>";
+        return;
+      }
+    }
+  }
+
+}
---
+++ front/networkHandler.js
@@ -0,0 +1,77 @@
+class NetworkHandler {
+
+  static hslRealTimeAPIHandler() {
+    if (this.status == 200 && this.responseText) {
+      tripData = NetworkHandler.parseHSLRealTimeData(this.responseText);
+      var queryStr = `{
+        fuzzyTrip(route: "${tripData.line}", direction: ${tripData.direction}, date: "${tripData.date}", time: ${tripData.start})
+        {
+          gtfsId
+          stops
+          {
+            gtfsId
+            name
+          }
+          route
+          {
+            longName
+          }
+        }
+  }`;
+
+      var r = new XMLHttpRequest();
+      r.onload = hslTripQueryHandler;
+      r.open("POST", HSL_API, true);
+      r.setRequestHeader("Content-type", "application/graphql");
+      r.send(queryStr);
+    } else {
+      console.log("Connection to HSL real time API failed");
+    }
+  }
+
+  static parseHSLRealTimeData(str) {
+    var tmpobj = JSON.parse(str);
+    if (!tmpobj) {
+      return null;
+    }
+    tmpobj = tmpobj[Object.keys(tmpobj)[0]]["VP"];
+    var d = new Date(tmpobj.tst);
+    var strDate = d.getUTCFullYear();
+    var m = d.getMonth() + 1;
+    strDate += m < 10? "0" + m: m;
+    strDate += d.getUTCDate();
+    return {
+      vehicle: tmpobj.veh,
+      line: "HSL:" + tmpobj.line,
+      direction: tmpobj.dir - 1,
+      start: ((Math.floor(tmpobj.start / 100) * 60) + tmpobj.start % 100) * 60,
+      timeStr: tmpobj.tst,
+      date: strDate
+    }
+  }
+
+  static hslTripQueryHandler() {
+    if (this.status == 200 && this.responseText != null) {
+      var queryRes = JSON.parse(this.responseText).data.fuzzyTrip;
+      tripData.stops = queryRes.stops;
+      tripData.route = queryRes.route;
+      UI.renderStops(tripData);
+    } else {
+      console.log("Connection to HSL GraphQL service failed");
+    }
+  }
+
+  static getCurrentVehicleData() {
+    var r = new XMLHttpRequest();
+    r.open("GET", RT_API_URL + BUS_ID + "/"); // asynchronous by default
+    r.onload = NetworkHandler.hslRealTimeAPIHandler;
+    r.send();
+  }
+
+  static postDriverButton() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", STOP_API + "/stoprequests", true);
+    xhttp.send();
+  }
+
+}
